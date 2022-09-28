313c946035455f55211caeb02828ea3216a95c70
Carlos Vallejo <carlosvallejo.webmaster@gmail.com>
2022-09-28 11:15:20 +0200

    update

--- seoboost/pages/_app.js
+++ seoboost/pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import PrimarySearchAppBar from '../components/NavBar';
+import StickyFooter from '../components/Footer';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <PrimarySearchAppBar />
+      <Component {...pageProps} />
+      <StickyFooter />
+    </>
+  )
 }
 
 export default MyApp
