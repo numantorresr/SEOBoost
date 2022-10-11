26821239a1aa1e2c4cd5e55ca75253811864ce05
Numan Torres <numantorresr@gmail.com>
2022-10-11 11:12:32 +0200

    ddbb configuration with heroku

--- seoboost/services/initAxios.js
+++ seoboost/services/initAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 class InitAxios {
     constructor(path) {
         this.axios = axios.create({
-            baseURL: `http://localhost:5005/api${path}`
+            baseURL: `https://seoboost.herokuapp.com/api${path}`
         })
     }
 }
